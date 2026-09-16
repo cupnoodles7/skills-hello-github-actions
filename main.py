ca125ca6a2a26e86bb61423b36bf6b179d373675
cupnoodles7 <akritikhetan7.sk@gmail.com>
2026-09-16 11:44:19 +0530

    install dependencies

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import requests
+
+import requests
+
+response = requests.get("https://api.github.com")
+print(response.status_code)  # 200 if it worked
+print(response.json())       # the response data
+
 def add(a, b):
     return a + b
 
